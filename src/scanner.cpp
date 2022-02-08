e5547ed8fe0c1cef981b9f41c11e5958580a228c
Asher <51806917+asher-lab@users.noreply.github.com>
2022-02-08 17:27:45 +0800

    Update scanner.cpp

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -7,14 +7,50 @@
 
 
 #include "scanner.hpp"
+#include <iomanip>
+#include <iostream>
+#include <cctype>
+#include <iostream>
+#include <fstream>
 
+
+using namespace std;
 using std::cout;
 using std::vector;
+using std::cerr;
+using std::endl;
+using std::ofstream;
+
+
+
+// tableMaker
+struct TableFormat {
+    int width;
+    char fill;
+    TableFormat(): width(14), fill(' ') {}
+    template<typename T>
+    TableFormat& operator<<(const T& data) {
+        std::cout << data << std::setw(width) << std::setfill(fill);
+        return *this;
+    }
+    TableFormat& operator<<(std::ostream&(*out)(std::ostream&)) {
+        std::cout << out;
+        return *this;
+    }
+};
+
+
 
 bool isID(const std::string &str)
 {
   if(std::isdigit(str[0]))
     return false;
+  
+  //if str[0] is a special symbol then return false
+   //if(!(isalpha(str[0])))
+    //return false;
+
+
   int counter = 0;
   if(str[0] == '_')
     counter++;
@@ -41,25 +77,74 @@ bool isString(const std::string &str)
   return str[0] == '"' && str[str.size()-1] == '"';
 }
 
+bool isStringSingle(const std::string &str)
+{
+  return str[0] == '\'' && str[str.size()-1] == '\'';
+}
+
+
+bool isStringParenthesis(const std::string &str)
+{
+  return ((str[1] == '"' && str[str.size()-2] == '"')&& ( str[0] == '(' &&  str[str.size()-1] == ')')) ;
+}
+
+bool isStringParenthesisSingle(const std::string &str)
+{
+  return str[1] == '\'' && str[str.size()-2] == '\'' && str[0] == '(' &&  str[str.size()-1] == ')' ;
+}
+
+
+
 bool isBool(const std::string &str)
 {
   return str == "true" || str == "false";
 }
 
+// call is string here
 bool isLiteral(const std::string &str)
 {
-  return isDigit(str) || isString(str) || isBool(str);
+//  return isDigit(str) || isString(str) || isStringSingle(str) || isStringParenthesis || isBool(str);
+return isDigit(str) || isString(str) || isBool(str) ||  isStringSingle(str) || isStringParenthesis(str) || isStringParenthesisSingle(str)  ;
 }
+
 
 bool isKeyword(const std::string &str)
 {
-  const vector<std::string> keywords{"int", "float", "auto", "double", "do", "switch", "return"};
+  const vector<std::string> keywords{"bool", "char", "default", "double", "drop", "else", "float", "high", "if", "inp", "int", "low", "out", "pow", "term", "until"};
   for(const auto& keyword : keywords)
     if (keyword == str)
       return true;
 
   return false;
 }
+
+
+//isKeywordNoiseWords
+bool isKeywordNoiseWords(const std::string &str)
+{
+  const vector<std::string> keywords{"input", "integer", "output", "power"};
+  for(const auto& keyword : keywords)
+    if (keyword == str)
+      return true;
+
+  return false;
+}
+
+
+//isKeywordReserved
+bool isKeywordReserved(const std::string &str)
+{
+  const vector<std::string> keywords{"body", "invalid", "next", "valid"};
+  for(const auto& keyword : keywords)
+    if (keyword == str)
+      return true;
+
+  return false;
+}
+
+
+
+
 
 bool isStatement(const std::string &str)
 {
@@ -71,9 +156,11 @@ bool isStatement(const std::string &str)
   return false;
 }
 
+//RelationalOperators
 bool isOperator(const std::string &str)
 {
-  const vector<std::string> operators{"<", ">", "<=", ">=", "*", "+", "-", "/", "=", "-=", "*=", "+=", "/=", "++", "--", "=="};
+  const vector<std::string> operators{"==", ">", "<", ">=", "<=",  "!=", "+", "-", "!", "/", "%", "*"};
+  //const vector<std::string> operators{"<", ">", "<=", ">=",  "==", "!="};
   for(const auto& op : operators)
     if (op == str)
       return true;
@@ -81,9 +168,43 @@ bool isOperator(const std::string &str)
   return false;
 }
 
+//ArithmeticOperators
+bool isOperatorArithmetic(const std::string &str)
+{
+  const vector<std::string> operators{"+","-","/","*","%","++","--"};
+  for(const auto& op : operators)
+    if (op == str)
+      return true;
+
+  return false;
+}
+//AssignmentOperators
+bool isOperatorAssignment(const std::string &str)
+{
+  const vector<std::string> operators{"=", "-=", "*=", "+=", "/=", "%="};
+  for(const auto& op : operators)
+    if (op == str)
+      return true;
+
+  return false;
+}
+
+//LogicalOperators
+bool isOperatorLogical(const std::string &str)
+{
+  const vector<std::string> operators{"!", "&&", "||"};
+  for(const auto& op : operators)
+    if (op == str)
+      return true;
+
+  return false;
+}
+
+
+
 bool isSeparator(const std::string &str)
 {
-  const vector<std::string> Separators{"{", "}", ",", "(", ")", ";"};
+  const vector<std::string> Separators{"{", "}", ",", "(", ")", ";", "[", "]"};
   for(const auto& separate : Separators)
     if (separate == str)
       return true;
@@ -98,23 +219,118 @@ bool isNotLegal(const std::string &str)
 
 void printRoleOfToken(const std::string& token)
 {
-  if(isOperator(token))
-    cout << "(operator, " << token << ")";
-  else if(isSeparator(token))
-    cout << "(separator, " << token << ")";
-  else if(isKeyword(token))
-    cout << "(keyword, " << token << ")";
-  else if(isStatement(token))
-    cout << "(statement, " << token << ")";
-  else if(isLiteral(token))
-    cout << "(literal, " << token << ")";
-  else if(isID(token))
-    cout << "(identifier, " << token << ")";
-  else if(isComment(token))
-    cout << "(comment, " << token << ")";
+TableFormat out;
+out.width = 8;
+ofstream outdatav2;
+ 
+if(isOperatorArithmetic(token)){
+    out << "Operators - Arithmetic:  " << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Operators - Arithmetic:  " << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+   }
+
+	
+  else if(isOperator(token)){
+    out << "Operators - Relational:  " << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Operators - Relational:  " << token << " "  << endl ;
+   outdatav2.close();
+    std::cout << "\n";
+	}
+	
+ 
+   
+  else if(isOperatorAssignment(token)){
+    out << "Operators - Assignment:  " << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Operators - Assignment:  " << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+   }
+   
+   
+  else if(isOperatorLogical(token)){
+    out << "Operators - Logical:   " << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Operators - Logical:  " << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+   }
+   
+  else if(isSeparator(token)){
+    out << "Delimeters:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Delimeters: \t\t " << token << " "  << endl ;
+    outdatav2.close(); 
+   std::cout << "\n";
+   }
+
+  //isKeyword	
+  else if(isKeyword(token)){
+    out << "Keywords:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Keywords:  \t\t" << token << " " <<  endl ;
+    outdatav2.close();
+    std::cout << "\n";
+	}
+	
+
+	
+  //isKeywordReserved	
+  else if(isKeywordReserved(token)){
+    out << "Reserved Keywords:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Reserved Keywords:  \t" << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+	}
+	
+  //isKeywordNoiseWords
+  else if(isKeywordNoiseWords(token)){
+    out << "NoiseWords Keywords:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "NoiseWords Keywords: \t" << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+	}
+	
+
+  else if(isID(token)){
+    out << "Identifiers:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Identifiers:  \t\t" << token << " "  << endl ;
+    outdatav2.close();
+    std::cout << "\n";
+        }
+
+
+	
+  else if(isLiteral(token)){
+    out << "Literals:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Literals:  \t\t" << token << " "  << endl ;
+    outdatav2.close();	
+    std::cout << "\n";
+	}
+
+
+	
+  else if(isComment(token)){
+    out << "Comments:   \t\t" << token << " ";
+    outdatav2.open("SymbolTable.txt", std::ios_base::app);
+    outdatav2 << "Comments:  \t\t" << token << " "  << endl ;
+    outdatav2.close();
+   std::cout << "\n";
+	}
+
+
+	
   else
     throw std::runtime_error("Invalid token: " + token);
-}
+} //end of printRoleOfToken
+
 
 void lexicalAnalyze(const std::string &nameOfFile)
 {
@@ -185,7 +401,7 @@ void lexicalAnalyze(const std::string &nameOfFile)
       continue;
     }
 
-    if(isOperator(std::string(1, ch)) && !isOperator(buffer))
+    if(isOperatorArithmetic(std::string(1, ch)) && !isOperatorArithmetic(buffer))
     {
       if(!buffer.empty())
       {
@@ -194,7 +410,9 @@ void lexicalAnalyze(const std::string &nameOfFile)
       }
     }
 
-    if(!isOperator(std::string(1, ch)) && isOperator(buffer))
+
+
+    if(!isOperatorArithmetic(std::string(1, ch)) && isOperatorArithmetic(buffer))
     {
       printRoleOfToken(buffer);
       buffer = "";
